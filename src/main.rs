ad01bea62a3ba99a3d29f9e9aa686719ebc08db1
phoexer <michael@musangeya.com>
2022-08-07 20:11:49 +0200

    add simple ping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use futures::StreamExt;
+use libp2p::{identity, Multiaddr, PeerId, Swarm};
+use libp2p::ping::{Ping, PingConfig};
+use libp2p::swarm::SwarmEvent;
+
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Hello, world!, peer id is {}", local_peer_id);
+
+    let transport = libp2p::development_transport(local_key).await?;
+
+    let behaviour = Ping::new(PingConfig::new().with_keep_alive(true));
+
+    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
+
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {}", addr)
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr {address, ..} => println!("Listening on {:?}", address),
+            SwarmEvent::Behaviour(event) => println!("{:?}", event),
+            _ => {}
+        }
+    }
 }
